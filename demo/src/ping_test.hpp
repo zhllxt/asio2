9b33223106ec2786f5565f05032466f1de790a15
zhllxt <37792738@qq.com>
2019-12-03 16:00:57 +0800

    Update ping_test.hpp

--- demo/src/ping_test.hpp
+++ demo/src/ping_test.hpp
@@ -37,8 +37,8 @@ public:
 	}
 	void run()
 	{
-		if (!ping.start("fe80::59f9:25af:4427:fab7"))
-			//if (!ping.start("123.45.67.89"))
+		if (!ping.start("123.45.67.89"))
+			//if (!ping.start("fe80::59f9:25af:4427:fab7")) //not supported yet
 			//if (!ping.start("stackoverflow.com"))
 			printf("start failure : %s\n", asio2::last_error_msg().c_str());
 
