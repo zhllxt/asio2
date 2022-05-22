65ba57010a85f89a5cd31e976e0406eeceecd9d4
怀良 张 <37792738@qq.com>
2022-05-22 21:53:24 +0800

    update shared_iopool example to add multi clients

--- example/shared_iopool/shared_iopool.cpp
+++ example/shared_iopool/shared_iopool.cpp
@@ -12,7 +12,7 @@ int main()
 
 	asio2::timer timer(iopool.get(std::rand() % iopool.size()));
 
-	timer.start_timer(1, std::chrono::milliseconds(1500), [&]()
+	timer.start_timer(1, std::chrono::milliseconds(1000), [&]()
 	{
 		printf("timer 1, loop infinite\n");
 	});
@@ -21,15 +21,19 @@ int main()
 
 	asio2::tcp_server server(std::vector<asio2::io_t*>{ &iopool.get(0), &iopool.get(1) });
 
-	server.bind_recv([&](std::shared_ptr<asio2::tcp_session>& session_ptr, std::string_view s)
+	server.bind_recv([&](std::shared_ptr<asio2::tcp_session>& session_ptr, std::string_view data)
 	{
 		ASIO2_ASSERT(session_ptr->io().strand().running_in_this_thread());
 
-		printf("recv : %zu %.*s\n", s.size(), (int)s.size(), s.data());
+		printf("recv : %zu %.*s\n", data.size(), (int)data.size(), data.data());
 
-		session_ptr->async_send(s);
+		// delay 3 seconds, then send the response
+		session_ptr->post([session_ptr, s = std::string{ data }]() mutable
+		{
+			session_ptr->async_send(std::move(s));
+		}, std::chrono::milliseconds(3000));
 
-	}).bind_connect([&](auto & session_ptr)
+	}).bind_connect([&](auto& session_ptr)
 	{
 		ASIO2_ASSERT(iopool.running_in_thread(0));
 
@@ -76,9 +80,34 @@ int main()
 
 	//-----------------------------------------------------------------------------------
 
+	std::vector<std::shared_ptr<asio2::tcp_client>> clients;
+
+	for (int i = 0; i < 10; i++)
+	{
+		std::shared_ptr<asio2::tcp_client>& client_ptr = clients.emplace_back(
+			std::make_shared<asio2::tcp_client>(iopool.get(i % iopool.size())));
+
+		client_ptr->bind_recv([client_ptr](std::string_view data)
+		{
+			client_ptr->async_send(data);
+		}).bind_connect([client_ptr]()
+		{
+			client_ptr->async_send("<0123456789abcdefg>");
+		});
+
+		client_ptr->async_start("127.0.0.1", 4567);
+	}
+
 	while (std::getchar() != '\n');  // press enter to exit this program
 
+	/* call the server.stop and client.stop, or not call the server.stop and client.stop, is OK.*/
+
 	//server.stop();
+
+	//for (auto& client_ptr : clients)
+	//{
+	//	client_ptr->stop();
+	//}
 
 	// must call iopool.stop() before exit.
 	iopool.stop();
