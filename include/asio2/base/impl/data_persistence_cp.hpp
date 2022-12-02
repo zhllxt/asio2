0ebd41005ed1528cb84df9fae97fafc4ac174774
怀良 张 <37792738@qq.com>
2022-12-02 12:41:40 +0800

    add _data_filter before send data

--- include/asio2/base/impl/data_persistence_cp.hpp
+++ include/asio2/base/impl/data_persistence_cp.hpp
@@ -48,28 +48,41 @@ namespace asio2::detail
 		~data_persistence_cp() = default;
 
 	protected:
+		/**
+		 * @brief Pre process the data before send it.
+		 * You can overload this function in a derived class to implement additional
+		 * processing of the data. eg: encrypt data with a custom encryption algorithm.
+		 */
+		template<class T>
+		inline auto _data_filter(T&& data)
+		{
+			return std::forward<T>(data);
+		}
+
 		template<class T>
 		inline auto _data_persistence(T&& data)
 		{
 			using data_type = detail::remove_cvref_t<T>;
+			
+			derived_t& derive = static_cast<derived_t&>(*this);
 
 			// std::string_view
 			if constexpr /**/ (detail::is_string_view_v<data_type>)
 			{
 				using value_type = typename data_type::value_type;
-				return std::basic_string<value_type>(data.data(), data.size());
+				return derive._data_filter(std::basic_string<value_type>(data.data(), data.size()));
 			}
 			// char* , const char* , const char* const
 			else if constexpr (detail::is_char_pointer_v<data_type>)
 			{
 				using value_type = typename detail::remove_cvref_t<std::remove_pointer_t<data_type>>;
-				return std::basic_string<value_type>(std::forward<T>(data));
+				return derive._data_filter(std::basic_string<value_type>(std::forward<T>(data)));
 			}
 			// char[]
 			else if constexpr (detail::is_char_array_v<data_type>)
 			{
 				using value_type = typename detail::remove_cvref_t<std::remove_all_extents_t<data_type>>;
-				return std::basic_string<value_type>(std::forward<T>(data));
+				return derive._data_filter(std::basic_string<value_type>(std::forward<T>(data)));
 			}
 			// object like : std::string, std::vector
 			else if constexpr (
@@ -80,14 +93,14 @@ namespace asio2::detail
 				std::is_trivially_move_assignable_v   <data_type> ||
 				std::is_nothrow_move_assignable_v     <data_type> )
 			{
-				return std::forward<T>(data);
+				return derive._data_filter(std::forward<T>(data));
 			}
 			else
 			{
 				// PodType (&data)[N] like : int buf[5], double buf[5]
 				auto buffer = asio::buffer(data);
-				return std::string(reinterpret_cast<const std::string::value_type*>(
-					const_cast<const void*>(buffer.data())), buffer.size());
+				return derive._data_filter(std::string(reinterpret_cast<const std::string::value_type*>(
+					const_cast<const void*>(buffer.data())), buffer.size()));
 			}
 		}
 
@@ -95,80 +108,108 @@ namespace asio2::detail
 		inline auto _data_persistence(CharT * s, SizeT count)
 		{
 			using value_type = typename detail::remove_cvref_t<CharT>;
-			return std::basic_string<value_type>(s, count);
+			
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(std::basic_string<value_type>(s, count));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(asio::const_buffer& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(const asio::const_buffer& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(asio::const_buffer&& data) noexcept
 		{
-			return std::move(data); // "data" is rvalue reference, should use std::move()
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			// "data" is rvalue reference, should use std::move()
+			return derive._data_filter(std::move(data));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(asio::mutable_buffer& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(const asio::mutable_buffer& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(asio::mutable_buffer&& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(std::move(data)));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(std::move(data))));
 		}
 
 #if !defined(ASIO_NO_DEPRECATED) && !defined(BOOST_ASIO_NO_DEPRECATED)
 		template<typename = void>
 		inline auto _data_persistence(asio::const_buffers_1& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(const asio::const_buffers_1& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(asio::const_buffers_1&& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(std::move(data)));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(std::move(data))));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(asio::mutable_buffers_1& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(const asio::mutable_buffers_1& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(data));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(data)));
 		}
 
 		template<typename = void>
 		inline auto _data_persistence(asio::mutable_buffers_1&& data) noexcept
 		{
-			return static_cast<derived_t&>(*this)._data_persistence(asio::const_buffer(std::move(data)));
+			derived_t& derive = static_cast<derived_t&>(*this);
+
+			return derive._data_filter(derive._data_persistence(asio::const_buffer(std::move(data))));
 		}
 #endif
 
